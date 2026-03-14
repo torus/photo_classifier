0a9c52bbe5676964f6cf3e940b52826b0bff9397
Toru Hisai <toru@torus.jp>
2026-03-14 11:53:57 +0900

    fix indentation

--- query_photos_by_city.py
+++ query_photos_by_city.py
@@ -18,9 +18,9 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
     
     dlat = lat2 - lat1
-dlon = lon2 - lon1
+    dlon = lon2 - lon1
     
-a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * atan2(sqrt(a), sqrt(1-a))
     distance = R * c
     
